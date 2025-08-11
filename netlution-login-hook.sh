d6fb90aac15fc832f373c9699797073efffad54e
junet-1 <wolf@junet.io>
2025-08-11 11:23:42 +0200

    Update netlution-login-hook.sh

--- netlution-login-hook.sh
+++ netlution-login-hook.sh
@@ -5,10 +5,11 @@ if [ -n "$DISPLAY" ] && [ "$USER" != "root" ] && [ ! -f "$HOME/.netlution-setup-
   wget -O "$HOME/netlution-setup.sh" "https://raw.githubusercontent.com/DEIN-REPO/BRANCH/netlution-setup.sh" 2>/dev/null
   if [ $? -eq 0 ]; then
     mkdir -p "$HOME/.config/autostart"
-    wget -O "$HOME/.config/autostart/netlution-setup.desktop" "https://raw.githubusercontent.com/DEIN-REPO/BRANCH/netlution-setup.desktop" 2>/dev/null
+    wget -O "$HOME/.config/autostart/netlution-setup.desktop" "https://raw.githubusercontent.com/junet-1/Intune-Linux/refs/heads/main/netlution-setup.desktop" 2>/dev/null
     chmod +x "$HOME/netlution-setup.sh"
     touch "$HOME/.netlution-setup-done"
     # Netlution Setup sofort starten
     "$HOME/netlution-setup.sh" &
   fi
+
 fi
